d8351f72294499e0f1d4fe8ed107143c82311612
leerivera <leerivera80@gmail.com>
2022-03-17 20:46:48 -0400

    first2

--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+
+function mulitEach(arr){
+    let product = 1
+    arr.forEach( num => product *= num )
+    alert(product)
+}
+
+mulitEach([10,7,9])
