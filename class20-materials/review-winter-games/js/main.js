23a2cb9f2a91843620f768fc4fc7e1bc170f5d50
leerivera <leerivera80@gmail.com>
2022-03-17 21:23:56 -0400

    first5

--- class20-materials/review-winter-games/js/main.js
+++ class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,17 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function takesArray(nums){
+      
+      let evens = []
+
+      nums.forEach( n => {
+          if(n % 2 === 0){
+              evens.push(n)
+              
+
+          }
+      })
+      return evens
+}
+
+console.log(takesArray([1,2,4,9,7]))
